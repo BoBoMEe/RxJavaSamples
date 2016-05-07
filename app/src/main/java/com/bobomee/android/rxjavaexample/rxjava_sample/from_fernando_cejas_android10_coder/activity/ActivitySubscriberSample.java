ec601117bcea9bbbda7b46bb25460d7bbb346ca6
BoBoMEe <wbwjx115@gmail.com>
2016-05-07 23:51:23 +0800

    add a  sample

---
+++ app/src/main/java/com/bobomee/android/rxjavaexample/rxjava_sample/from_fernando_cejas_android10_coder/activity/ActivitySubscriberSample.java
@@ -0,0 +1,75 @@
+package com.bobomee.android.rxjavaexample.rxjava_sample.from_fernando_cejas_android10_coder.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.bobomee.android.rxjavaexample.BaseActivity;
+import com.bobomee.android.rxjavaexample.R;
+import com.bobomee.android.rxjavaexample.rxjava_sample.from_fernando_cejas_android10_coder.adapter.ElementsAdapter;
+import com.bobomee.android.rxjavaexample.rxjava_sample.from_fernando_cejas_android10_coder.data.DataManager;
+import com.bobomee.android.rxjavaexample.rxjava_sample.from_fernando_cejas_android10_coder.data.NumberGenerator;
+import com.bobomee.android.rxjavaexample.rxjava_sample.from_fernando_cejas_android10_coder.data.StringGenerator;
+import com.bobomee.android.rxjavaexample.rxjava_sample.from_fernando_cejas_android10_coder.executor.JobExecutor;
+
+import butterknife.Bind;
+import butterknife.OnClick;
+import rx.Subscription;
+import rx.subscriptions.Subscriptions;
+
+/**
+ * Created by bobomee on 16/5/7.
+ */
+public class ActivitySubscriberSample extends BaseActivity implements ElementsAdapter.ElementAddedListener {
+    @Bind(android.R.id.list)
+    RecyclerView rv_elements;
+
+    private DataManager dataManager;
+    private ElementsAdapter adapter;
+
+    private Subscription subscription;
+
+    public static Intent getCallingIntent(Context context) {
+        return new Intent(context, ActivitySubscriberSample.class);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_subscriber);
+
+        initialize();
+        fillData();
+    }
+
+    @Override
+    protected void onDestroy() {
+        subscription.unsubscribe();
+        super.onDestroy();
+    }
+
+    private void initialize() {
+        subscription = Subscriptions.unsubscribed();
+        dataManager = new DataManager(new StringGenerator(), new NumberGenerator(),
+                JobExecutor.getInstance());
+        adapter = new ElementsAdapter(this, this);
+        rv_elements.setLayoutManager(new LinearLayoutManager(this));
+        rv_elements.setAdapter(this.adapter);
+    }
+
+    private void fillData() {
+        subscription = this.dataManager.elements().subscribe(this.adapter);
+    }
+
+    @OnClick(android.R.id.button1) void onAddElementClick() {
+        subscription = this.dataManager.newElement().subscribe(this.adapter);
+        Toast.makeText(this, "Element added using an observable!!!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override public void onElementAdded() {
+        rv_elements.smoothScrollToPosition(adapter.getItemCount());
+    }
+}
---
+++ app/src/main/java/com/bobomee/android/rxjavaexample/rxjava_sample/from_fernando_cejas_android10_coder/data/DataManager.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) 2015 android10.org Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bobomee.android.rxjavaexample.rxjava_sample.from_fernando_cejas_android10_coder.data;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.schedulers.Schedulers;
+
+public class DataManager {
+
+  private final StringGenerator stringGenerator;
+  private final NumberGenerator numberGenerator;
+  private final Executor jobExecutor;
+
+  public DataManager(StringGenerator stringGenerator,
+      NumberGenerator numberGenerator, Executor jobExecutor) {
+    this.stringGenerator = stringGenerator;
+    this.numberGenerator = numberGenerator;
+    this.jobExecutor = jobExecutor;
+  }
+
+  public Observable<Integer> numbers() {
+    return Observable.from(numberGenerator.numbers());
+  }
+
+  public Observable<Long> milliseconds() {
+    return Observable.interval(0, 1, TimeUnit.MILLISECONDS);
+  }
+
+  public Observable<Integer> squareOfAsync(int number) {
+    return Observable.just(number * number).subscribeOn(Schedulers.from(jobExecutor));
+  }
+
+  public Observable<String> elements() {
+    return Observable.from(stringGenerator.randomStringList());
+  }
+
+  public Observable<String> newElement() {
+    return Observable
+        .just(stringGenerator.nextString())
+        .map((string -> "RandomItem" + string));
+  }
+
+  public List<Integer> getNumbersSynchronously() {
+    return numberGenerator.numbers();
+  }
+}
